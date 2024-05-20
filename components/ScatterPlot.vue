21ec46b8af69c91ba7d42493f152e0852b40b8fb
avalle <ani.valle@bsc.es>
2024-05-20 12:47:43 +0200

    Add template with syntax vuetify

--- components/ScatterPlot.vue
+++ components/ScatterPlot.vue
@@ -1,10 +1,63 @@
 <template>
-  <v-card>
-    <v-card-title>Scatter Plot</v-card-title>
-    <v-card-text>
-      <div ref="chart"></div>
-    </v-card-text>
-  </v-card>
+    <v-container>
+    <v-row>
+      <v-col cols="8">
+        <div class="butns">
+
+          <!-- Buttons -->
+          <v-btn-toggle class="custom-btn-toggle">
+            <v-btn outlined>
+              Sort & Classify Data
+            </v-btn outlined>
+            <v-btn>
+              Optimal View
+            </v-btn>
+            <!-- Dropdown for Download -->
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn outlined v-bind="attrs" v-on="on">
+                  Download
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item class="menu-item">
+                  <v-list-item-title>SVG</v-list-item-title>
+                </v-list-item>
+                <v-list-item class="menu-item">
+                  <v-list-item-title>PNG</v-list-item-title>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item class="menu-item">
+                  <v-list-item-title>PDF</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <!-- End of Dropdown for Download -->
+          </v-btn-toggle>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-4">
+      <!-- Chart -->
+      <v-col cols="8" id="chartCapture">
+        <div ref="chart" id="scatterPlot"></div>
+        <br>
+        <!-- ID AND DATE TABLE -->
+        <v-simple-table class="custom-table">
+          <tbody>
+                <tr>
+                  <th class="first-th">Dataset ID</th>
+                  <td>{{ datasetId }}</td>
+                  <th>Last Update</th>
+                  <td class="last-td">{{ formatDateString(datasetModDate) }}</td>
+                </tr>
+              </tbody>
+        </v-simple-table>
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
