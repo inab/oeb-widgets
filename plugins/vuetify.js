638994b2883165379a4fbdb59ad9ad732fac3259
avalle <ani.valle@bsc.es>
2024-05-27 12:39:50 +0200

    Add new settings to styles

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,4 +3,24 @@ import Vuetify from 'vuetify'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107',
+          },
+        },
+        options: {
+          customProperties: true,
+        },
+      },
+      typography: {
+        fontFamily: 'Roboto, sans-serif',
+      },
+})
