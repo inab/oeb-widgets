f7dcf8affdd4434d2eb8e5e7cdd319f86d621d9d
Andrea <andrea.morales@bsc.es>
2024-05-28 17:18:36 +0200

    Fetched another dataset

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@ export default {
   },
   async mounted() {
     // Fetch your data
-    const response = await fetch('./test.json');
+    const response = await fetch('./OEBD004000000D.json');
     //const response = await fetch('./raw_data_OEBD00200002UK0.json');
     this.fetchedData = await response.json();
     this.fetchDataAndRender(this.fetchedData)
